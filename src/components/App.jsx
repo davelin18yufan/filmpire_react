9088382a2c3665e63fdf3de55ad38ab9da5eed4e
davelin18yufan <davelin30630@gmail.com>
2023-11-16 16:18:29 +0800

    update router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,38 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Box from '@mui/system/Box';
 import { Actors, Profile, MovieInformation, Movies, NavBar } from '.';
 
-const App = () => (
-  <Box sx={{
-    display: 'flex',
-    height: '100%' }}
+const Root = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      height: '100%',
+    }}
   >
     <CssBaseline />
     <NavBar />
-    <Box
-      component="main"
-      sx={{ flexGrow: '1',
-        p: '2rem' }}
-    >
+    <Box component="main" sx={{ flexGrow: '1', p: '2rem' }}>
       <Box sx={{ height: '70px' }} />
-      <Routes>
-        <Route exact path="/movie/:id" element={<MovieInformation />} />
-
-        <Route
-          exact
-          path="/actors/:id"
-          element={<Actors />}
-        />
-
-        <Route
-          exact
-          path="/profile/:id"
-          element={<Profile />}
-        />
-
-        <Route
-          exact
-          path="/"
-          element={<Movies />}
-        />
-      </Routes>
+      <Outlet />
     </Box>
   </Box>
 );
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      { path: '', element: <Movies /> },
+      { path: 'movie/:id', element: <MovieInformation /> },
+      { path: 'profile/:id', element: <Profile /> },
+      { path: 'actors/:id', element: <Actors /> },
+    ],
+  },
+]);
+
+const App = () => <RouterProvider router={router} />;
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App';
 
-ReactDom.render(
-  <BrowserRouter>
+ReactDom.createRoot(document.querySelector('#root')).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.querySelector('#root'),
+  </React.StrictMode>,
 );
