b47b5d804eda0cad451113e95b6b1e616384c8ca
davelin18yufan <davelin30630@gmail.com>
2023-11-16 14:37:15 +0800

    config routes and project structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+import { Actors, Profile, MovieInformation, Movies, NavBar } from '.';
 
 const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Switch>
+        <Route exact path="/movie/:id">
+          <MovieInformation />
+        </Route>
+
+        <Route exact path="/actors/:id">
+          <Actors />
+        </Route>
+
+        <Route exact path="/profile/:id">
+          <Profile />
+        </Route>
+
+        <Route exact path="/">
+          <Movies />
+        </Route>
+      </Switch>
+    </main>
+  </div>
 );
 
 export default App;
