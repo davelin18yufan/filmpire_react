07dc35bd5f176b3b5e6cc944c72756582630f8c4
davelin18yufan <davelin30630@gmail.com>
2023-11-16 15:35:27 +0800

    add styles in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Box from '@mui/system/Box';
 import { Actors, Profile, MovieInformation, Movies, NavBar } from '.';
 
 const App = () => (
-  <div>
+  <Box sx={{
+    display: 'flex',
+    height: '100%' }}
+  >
     <CssBaseline />
     <NavBar />
-    <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <MovieInformation />
-        </Route>
+    <Box
+      component="main"
+      sx={{ flexGrow: '1',
+        p: '2rem' }}
+    >
+      <Box sx={{ height: '70px' }} />
+      <Routes>
+        <Route exact path="/movie/:id" element={<MovieInformation />} />
 
-        <Route exact path="/actors/:id">
-          <Actors />
-        </Route>
+        <Route
+          exact
+          path="/actors/:id"
+          element={<Actors />}
+        />
 
-        <Route exact path="/profile/:id">
-          <Profile />
-        </Route>
+        <Route
+          exact
+          path="/profile/:id"
+          element={<Profile />}
+        />
 
-        <Route exact path="/">
-          <Movies />
-        </Route>
-      </Switch>
-    </main>
-  </div>
+        <Route
+          exact
+          path="/"
+          element={<Movies />}
+        />
+      </Routes>
+    </Box>
+  </Box>
 );
-
 export default App;
